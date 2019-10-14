9d6aca628c32d0975be4288d5b6cb340ede5569d
Vas-solo <33657511+Vas-solo@users.noreply.github.com>
2019-10-14 18:14:10 +0100

    midi-converter-v2

--- main.js
+++ main.js
@@ -1,36 +1,122 @@
 const electron = require('electron');
-const url = require('url');
 const path = require('path');
+const url = require('url');
 var     cmd=require('node-cmd');
-//console.log('yolo!');
 
-       //cmd.run('sendmidi dev MIDI Guitar channel 16 on 60 64');
+// SET ENV
+process.env.NODE_ENV = 'development';
 
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, Menu, ipcMain} = electron;
 
 let mainWindow;
+let addWindow;
+
+//function to get tapmode
+function tap(){
+
+  console.log('yolo!');
+
+  cmd.run('sendmidi dev MIDI Guitar channel 16 on 61 20');
+
+}
+//tap();
 // Listen for app to be ready
 app.on('ready', function(){
-    // Create new window
-    mainWindow = new BrowserWindow();
-    // Load HTML into Window 
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'mainWindow.html'),
-        protocol: 'file:',
-        slashes: true
-    }));
+  // Create new window
+  mainWindow = new BrowserWindow({});
+  // Load html in window
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'mainWindow.html'),
+    protocol: 'file:',
+    slashes:true
+  }));
+  // Quit app when closed
+  mainWindow.on('closed', function(){
+    app.quit();
+  });
 
-    //tap();
-
-    // Function to change to Tap mpode
-
-    function tap(){
-
-      // document.getElementById("demo");
-        console.log('yolo!');
-
-      cmd.run('sendmidi dev MIDI Guitar channel 16 on 60 64');
-
-   }
-
+  // Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  // Insert menu
+  Menu.setApplicationMenu(mainMenu);
 });
+
+// Handle add item window
+function createAddWindow(){
+  addWindow = new BrowserWindow({
+    width: 300,
+    height:200,
+    title:'Add Shopping List Item'
+  });
+  addWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'addWindow.html'),
+    protocol: 'file:',
+    slashes:true
+  }));
+  // Handle garbage collection
+  addWindow.on('close', function(){
+    addWindow = null;
+  });
+}
+
+// Catch item:add
+ipcMain.on('item:add', function(e, item){
+  mainWindow.webContents.send('item:add', item);
+  addWindow.close(); 
+  // Still have a reference to addWindow in memory. Need to reclaim memory (Grabage collection)
+  //addWindow = null;
+});
+
+// Create menu template
+const mainMenuTemplate =  [
+  // Each object is a dropdown
+  {
+    label: 'File',
+    submenu:[
+      {
+        label:'Add Item',
+        click(){
+          createAddWindow();
+            tap();
+        }
+      },
+      {
+        label:'Clear Items',
+        click(){
+          mainWindow.webContents.send('item:clear');
+        }
+      },
+      {
+        label: 'Quit',
+        accelerator:process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click(){
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+
+// If OSX, add empty object to menu
+if(process.platform == 'darwin'){
+  mainMenuTemplate.unshift({});
+}
+
+// Add developer tools option if in dev
+if(process.env.NODE_ENV !== 'production'){
+  mainMenuTemplate.push({
+    label: 'Developer Tools',
+    submenu:[
+      {
+        role: 'reload'
+      },
+      {
+        label: 'Toggle DevTools',
+        accelerator:process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+        click(item, focusedWindow){
+          focusedWindow.toggleDevTools();
+        }
+      }
+    ]
+  });
+}
